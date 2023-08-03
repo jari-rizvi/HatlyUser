53c9e1013357a0bbacbc6e3f180ec236f33fca74
Moin Akhtar <moinakhtar@teamx.global>
2023-08-03 16:35:35 +0500

    working on google signin

--- app/src/main/java/com/teamx/hatlyUser/ui/fragments/auth/login/LoginFragment.kt
+++ app/src/main/java/com/teamx/hatlyUser/ui/fragments/auth/login/LoginFragment.kt
@@ -1,13 +1,29 @@
 package com.teamx.hatlyUser.ui.fragments.auth.login
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.gson.JsonObject
 import com.teamx.hatlyUser.BR
 import com.teamx.hatlyUser.R
@@ -39,6 +55,8 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
     private var userPass: String? = null
     private var randNum: String? = null
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -50,6 +68,26 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
                 popExit = R.anim.nav_default_pop_exit_anim
             }
         }
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("42990662374-rh38rb24cfth35psqrrgcbqp67dnpctv.apps.googleusercontent.com").requestEmail().build()
+
+//        googleSignInClient = GoogleSignIn.getClient(requireActivity() , gso)
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
+
+
+//        val providers: List<AuthUI.IdpConfig> = listOf(
+//            AuthUI.IdpConfig.GoogleBuilder().build()
+//        )
+//
+//        val signInIntent: Intent = AuthUI.getInstance()
+//            .createSignInIntentBuilder()
+//            .setAvailableProviders(providers)
+//            .build()
+//        signInLauncher.launch(signInIntent)
 
         PrefHelper.getInstance(requireActivity()).setNotFirstTime(true)
 
@@ -68,6 +106,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
                 initialization()
                 mViewModel.login(createParams())
             }
+        }
+
+        mViewDataBinding.txtLoginGoogle.setOnClickListener {
+            signInGoogle()
         }
 
         if (!mViewModel.loginResponse.hasActiveObservers()) {
@@ -113,6 +155,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
             viewLifecycleOwner,
             onBackPressedCallback
         )
+
     }
 
     private fun initialization() {
@@ -148,11 +191,44 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
         return true
     }
 
-    fun generateRandom(): Long {
+    private fun generateRandom(): Long {
         val rangeStart = 1000000000000000000L
         val rangeEnd = 9000000000000000000L
 
         return Random.nextLong(rangeStart, rangeEnd)
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }else{
+                Log.d("onSignInResult", "else: ")
+            }
+        }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        } else {
+            Toast.makeText(requireContext(), task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+
+
+        Log.d("onSignInResult", "updateUI: ${account.idToken}")
     }
 
 }
